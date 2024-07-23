23233fdfa4172be8d3860ff09f2c77b03586bd99
annu2583 <annuverma8234@gmail.com>
2024-07-23 15:59:25 +0530

    portfolio

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function Projects() {
-<<<<<<< HEAD
     let info = [
         {
             img: 'folder.svg',
@@ -55,8 +54,6 @@ function Projects() {
 
 
     ]
-=======
->>>>>>> 59bb45e8742060ba30769d65cb3447adc8994227
     return (
         <>
             <div className='container'>
@@ -64,7 +61,6 @@ function Projects() {
                     <div className='about_heading'>
                         <div className='about_col' id='projects'>PROJECTS</div>
                     </div>
-<<<<<<< HEAD
                     <div className='projects_grid'>
                         {info?.map((index, item) => {
                             return (
@@ -90,69 +86,6 @@ function Projects() {
                             )
                         })}
                     </div>
-=======
-                    {/* project 1st */}
-                    <div className='project_flex'>
-                        <div className='project_icons'>
-                            <img src='folder.svg' />
-                            <div className='project_link'>
-                                <a href="https://github.com/annu2583/Portfolio.git" target="_blank">
-                                    <img className='links' src='git.svg' />
-                                </a>
-                                <a href='' target='_blank'>
-                                    <img className='links' src='external.svg' />
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className='project_info'>
-                            <h2>Calculator App</h2>
-                            <h4>HTML  CSS  Javascript</h4>
-                        </div>
-                    </div>
-
-                    {/* project 2nd */}
-                    <div className='project_flex'>
-                        <div className='project_icons'>
-                            <img src='folder.svg' />
-                            <div className='project_link'>
-                                <a href="https://github.com/annu2583/Portfolio.git" target="_blank">
-                                    <img className='links' src='git.svg' />
-                                </a>
-                                <a href='' target='_blank'>
-                                    <img className='links' src='external.svg' />
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className='project_info'>
-                            <h2>Digital Clock</h2>
-                            <h4>HTML  CSS  Javascript</h4>
-                        </div>
-                    </div>
-
-
-                    {/* project 2nd */}
-                    <div className='project_flex'>
-                        <div className='project_icons'>
-                            <img src='folder.svg' />
-                            <div className='project_link'>
-                                <a href="https://github.com/annu2583/Portfolio.git" target="_blank">
-                                    <img className='links' src='git.svg' />
-                                </a>
-                                <a href='' target='_blank'>
-                                    <img className='links' src='external.svg' />
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className='project_info'>
-                            <h2>Digital Clock</h2>
-                            <h4>HTML  CSS  Javascript</h4>
-                        </div>
-                    </div>
-
->>>>>>> 59bb45e8742060ba30769d65cb3447adc8994227
                 </div>
             </div>
         </>
